51c3a2a04b4aee7b6e071d85df1ec63d325700e2
Roman Kharitonov <refaim.vl@gmail.com>
2011-04-13 10:56:05 +1100

    Dijkstra: Handle errors

--- dijkstra/dijkstra.py
+++ dijkstra/dijkstra.py
@@ -12,6 +12,7 @@ UNARY_OPERATORS = {
 RESTORE_UNARY = dict(zip(UNARY_OPERATORS.itervalues(), UNARY_OPERATORS.iterkeys()))
 
 OPERATOR_PRIORITIES = {
+    'p':  (3, 'r'),
     'm':  (3, 'r'),
     '**': (2, 'r'),
     '*':  (1, 'l'),
@@ -40,7 +41,14 @@ TOKEN_RE = re.compile(
         )
     )''', re.VERBOSE)
 
-class DError(Exception): pass
+ALLOWED_SEQUENCES = {
+    lambda t: isop(t) or t == '(' or t is None:
+        lambda c: isnumber(c) or isunary(c) or c == '(',
+    lambda t: isnumber(t) or t == ')':
+        lambda c: isop(c) or c == ')',
+}
+
+class DijkstraError(Exception): pass
 
 
 def tokens(string):
@@ -54,7 +62,7 @@ def tokens(string):
         else:
             while string[pos].isspace():
                 pos += 1
-            raise DError('unknown char: %s' % string[pos])
+            raise DijkstraError('unexpected char: %s' % string[pos])
 
 
 def calculate(postfix):
@@ -63,7 +71,15 @@ def calculate(postfix):
         token = postfix.pop(0)
         if isop(token):
             args = [result.pop(), result.pop()][::-1]
-            result.append(CHAR2FUNC[token](*args))
+            try:
+                result.append(CHAR2FUNC[token](*args))
+            except (ValueError, OverflowError), ex:
+                raise DijkstraError(ex.args[-1])
+            except ZeroDivisionError, ex:
+                msg = ex.args[0]
+                if msg == 'float division':
+                    msg = 'division by zero'
+                raise DijkstraError(msg)
         else:
             result.append(token)
     return result[0]
@@ -81,19 +97,32 @@ def isleft(operator):
     return OPERATOR_PRIORITIES[operator][1] == 'l'
 
 
+def isunary(operator):
+    return operator in UNARY_OPERATORS
+
+
+def isnumber(token):
+    try:
+        float(token)
+    except (TypeError, ValueError):
+        return False
+    return True
+
+
 def main(argv):
     expression = (' '.join(argv) if argv else raw_input()).strip()
     if not expression:
-        return 0
+        return None
 
     stack, postfix = [], []
-    prev = '+' # dummy operator, only for first iteration
+    prev = None # dummy value, only for first iteration
     for token in tokens(expression):
+        for pred, rule in ALLOWED_SEQUENCES.iteritems():
+            if pred(prev) and not rule(token):
+                raise DijkstraError('wrong syntax')
 
         if isop(token):
-            if isop(prev) and token in UNARY_OPERATORS:
-                if token == '+':
-                    continue
+            if (isop(prev) or prev == None or prev == '(') and isunary(token):
                 postfix.append(0)
                 token = UNARY_OPERATORS[token]
 
@@ -108,10 +137,10 @@ def main(argv):
             stack.append(token)
 
         elif token == ')':
-            while stack[-1] != '(':
+            while stack and stack[-1] != '(':
                 postfix.append(stack.pop())
-                if not stack:
-                    raise DError('parentheses mismatch')
+            if not stack:
+                raise DijkstraError('parentheses mismatch')
             stack.pop()
 
         else:
@@ -119,10 +148,13 @@ def main(argv):
 
         prev = token
 
+    if isop(prev):
+        raise DijkstraError('wrong syntax')
+
     while stack:
         operator = stack.pop()
         if operator == '(':
-            raise DError('parentheses mismatch')
+            raise DijkstraError('parentheses mismatch')
         postfix.append(operator)
 
     result = calculate(map(lambda x: RESTORE_UNARY.get(x, x), postfix))
@@ -135,6 +167,6 @@ if __name__ == '__main__':
         sys.exit(main(sys.argv[1:]))
     except KeyboardInterrupt:
         print 'Interrupted by user'
-    except DError, ex:
-        print u'%s: error: %s' % (os.path.basename(__file__), ex.args[0])
+    except DijkstraError, ex:
+        print u'%s: error: %s' % (os.path.basename(__file__), ex.args[0].lower())
     sys.exit(1)
